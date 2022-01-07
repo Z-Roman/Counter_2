d5ccc91b2849984e6d8bf5b93a7a8b368cc255bd
Roman <r.zharuk@ukr.net>
2022-01-07 18:08:57 +0200

    методы

--- Counter.cpp
+++ Counter.cpp
@@ -1,1 +1,41 @@
 #include "Counter.h"
+
+Counter::Counter()
+{
+    value_min = 0;
+    value_max = 99999;
+    step = 1;
+    cur_count = 0;
+}
+
+Counter::Counter(unsigned int value_min, unsigned int value_max, unsigned int step, unsigned int cur_count)
+{
+    this->value_min = value_min;
+    this->value_max = value_max;
+    this->step = step;
+    this->cur_count = cur_count;
+}
+
+int Counter::Set_min(unsigned int value_min)
+{
+    this->value_min = value_min;
+}
+
+int Counter::Set_max(unsigned int value_max)
+{
+    this->value_max = value_max;
+}
+
+int Counter::Set_cur_count(unsigned int cur_count)
+{
+    this->cur_count = cur_count;
+}
+
+int Counter::Set_step(unsigned int step)
+{
+    this->step = step;
+}
+
+
+
+
--- Counter.h
+++ Counter.h
@@ -8,5 +8,19 @@ class Counter
 	unsigned int step;
 	unsigned int cur_count;
 
+public:
+
+	Counter();
+
+	Counter(unsigned int value_min, unsigned int value_max, unsigned int step, unsigned int cur_count);
+
+	int Set_min(unsigned int value_min);
+
+	int Set_max(unsigned int value_max);
+
+	int Set_cur_count(unsigned int cur_count);
+
+	int Set_step(unsigned int step);
+
 };
 
